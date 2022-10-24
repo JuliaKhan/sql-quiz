1e16b937e242cd367a54fc4ac0b5fee24c932ac9
“Julia <juliaabkhan@gmail.com>
2022-10-24 12:26:23 -0500

    Removed some lines at the end of database.sql that were causing an error.

--- database.sql
+++ database.sql
@@ -41563,10 +41563,6 @@ CREATE UNIQUE INDEX ix_salespeople_email ON salespeople USING btree (email);
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
 
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-REVOKE ALL ON SCHEMA public FROM postgres;
-GRANT ALL ON SCHEMA public TO postgres;
-GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
 --
